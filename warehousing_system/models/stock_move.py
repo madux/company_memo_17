f2b531890de8a5e0499ebe46608382cbc730405c
madux3 <kingstongerald@gmail.com>
2025-05-06 09:54:13 +0100

    [FIX] Fixes to resolve issues

--- warehousing_system/models/stock_move.py
+++ warehousing_system/models/stock_move.py
@@ -1,10 +1,14 @@
 from odoo import api, fields, models
+import io
+import base64
+import qrcode
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
     #item descrition = description_picking
     #length (in mtr) = 
+    is_label_printed = fields.Boolean(String="Label Printed")
     length_mtr = fields.Integer(String="Length (mtr)")
     #width (in mtr) =
     width_mtr = fields.Integer(String="Width (mtr)")
@@ -21,10 +25,29 @@ class StockMove(models.Model):
     area_m2 = fields.Float(String="Space occupied (m2)")
     #Space Occupied in m3
     volume_m3 = fields.Float(String="Space occupied (m3)")
-    #Chargeable Space in m2
     area_chargeable = fields.Float(String="Chargeable Space(m2)")
     
     show_details_visible = fields.Boolean('Details Visible', default=True)
     no_of_items = fields.Float('No. of items', help="No of item in each box")
     item_picture = fields.Binary("Picture")
-     
+    qr_code = fields.Binary(string="QR Code")
+    
+    def create_qr_code(self, code):
+        qr = qrcode.QRCode()
+        qr.add_data(code)
+        return qr.make_image(fill_color="black", back_color="white")
+    
+    def generate_qr_code(self):
+        img = self.create_qr_code(self.name)
+        buffered = io.BytesIO()
+        img.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        self.write({
+            'qr_code': img_str,
+        })
+        
+    def print_way_bill_item(self):
+        self.is_label_printed = True 
+        self.generate_qr_code()
+        return self.env.ref('warehousing_system.print_waybill_item_report').report_action(self)
+
